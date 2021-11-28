e239228b5befa5e75e1d6e6216afa46977bbaf7c
Miles Johnson <mgj2@cecs.pdx.edu>
2021-11-28 12:03:25 -0800

    updated main

--- main.cpp
+++ main.cpp
@@ -2,17 +2,46 @@
 #include "Edge.h"
 #include <vector>
 #include <fstream>
+#include <algorithm>
 
 using std::cin
 using std::cout
 using std::string
 using std::vector
+using std::map
 
 
 int main(){
+    const string fileName = "city-pairs.txt"
     vector<Edge> edgeArray;
-    readFromFile(edgeArray, "city-pairs.txt")
+    map<string, int> cityList;
+    assert (!buildCityList(cityList, fileName));
+    assert (!readFromFile(edgeArray, fileName));
 
+}
+
+bool buildCityList(vector<string> cityList, const string fileName){
+    ifstream infile (fileName);
+    if (!infile.isopen())
+        return false;
+    
+    string cityName;
+    string tmp;
+
+    /*The following code block reads in city names and maps them to integers (i).
+    * Each string read in is checked for uniqueness with the cityList.find()
+    * function before being added.  The iterator i is incremented whenever a
+    * unique city name is added to the map.
+    */
+    int i = 1;
+    do{
+        infile >> cityName;
+        infile.ignore(1000, '\n');
+        if (std::find(cityList.begin(), cityList.end(), cityName != cityList.end())){
+            cityList.pushback(cityName);
+            ++i;
+        }
+    }while (!infile.eof());
 }
 
 /*This function reads data from a plain text file and stores the data
@@ -21,18 +50,22 @@ int main(){
 * spaces.  If a vertex name is more than one word, the words are
 * delimited with a period.
 */
+
 bool readFromFile(vector<Edge> edgeArray, const string fileName){
     //<vector>.pushback() adds an element to the end of a vector
     ifstream infile;
-    infile.open("fileName");
+    infile.open(fileName);
     if (!infile.isopen()){
         return false;
     }
     string cityName;
     string destinationName;
     int distance;
+    infile << cityName;
 
     while (!infile.eof()){
-        infile
+        infile >> cityName >> destinationName >> distance;
+        infile.ignore(1000, '\n');
+
     }
 }
